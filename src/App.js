fb36fa481afb9f9e2e5a34f9603bffac0faa2082
Dawit Jiru <dawit.jiru@gmail.com>
2022-12-27 04:32:36 -0600

    main split screen css edited

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './main.css';
-import HomePage from './components/HomePage';
-import EmployeePage from './components/EmployeePage';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import EmployeeList from './components/EmployeeList';
-import EmployeeListItem from './components/EmployeeListItem';
-
+import "./main.css";
+import HomePage from "./components/HomePage";
+import EmployeePage from "./components/EmployeePage";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import EmployeeList from "./components/EmployeeList";
+import EmployeeListItem from "./components/EmployeeListItem";
 
 function App() {
   return (
-    <div className="App">
-    <h2> Hellooooooooooo from react</h2>
-    <HomePage />
-    <EmployeePage />
-    <Header />
-    <SearchBar />
-    <EmployeeList />
-    <EmployeeListItem />
+    <div className="content">
+      <div className="left">
+        <HomePage />
+        <EmployeePage />
+        <Header />
+        <SearchBar />
+      </div>
+      <div className="right">
+        <EmployeeList />
+        <EmployeeListItem />
+      </div>
     </div>
   );
 }
