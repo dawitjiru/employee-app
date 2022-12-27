9bac8a2a35d4c8bfbeab223ec1cb95dcdcf5da32
Dawit Jiru <dawit.jiru@gmail.com>
2022-12-27 03:37:10 -0600

    main split screen css added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './main.css';
 import HomePage from './components/HomePage';
 import EmployeePage from './components/EmployeePage';
 import Header from './components/Header';
