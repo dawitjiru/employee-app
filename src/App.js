5ba5e79a4150b2f366eb71bdce4a1429ed377e51
Dawit Jiru <dawit.jiru@gmail.com>
2023-01-02 18:03:08 -0600

    add images

--- src/App.js
+++ src/App.js
@@ -5,8 +5,41 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import EmployeeList from "./components/EmployeeList";
 import EmployeeListItem from "./components/EmployeeListItem";
+import imageOne from "./assets/images/image_one.png";
+import imageTwo from "./assets/images/image_two.png";
+import imageThree from "./assets/images/image_three.png";
 
 function App() {
+  const emplyees = [
+    {
+      id: 1,
+      image: imageOne,
+      name: "John Doe",
+      title: "Front end dev",
+      callMobile: "123578900",
+    },
+    {
+      id: 2,
+      image: imageTwo,
+      name: "Eric Johnatan",
+      title: "CTO",
+      callMobile: "918309834098",
+    },
+    {
+      id: 3,
+      image: imageThree,
+      name: "Metin Khaled",
+      title: "Team Lead",
+      callMobile: "838092382",
+    },
+    {
+      id: 4,
+      image: imageTwo,
+      name: "Rosa Ian",
+      title: "QA Lead",
+      callMobile: "098392380",
+    },
+  ];
   return (
     <div className="content">
       <div className="left">
