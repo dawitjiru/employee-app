69cbc070262aabfa4d5fe6bc0e0f6720002aa726
Dawit Jiru <dawit.jiru@gmail.com>
2023-02-03 01:12:53 -0600

    Updates

---
+++ precache-manifest.43e6bea596790c1b7929609c4e185f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13f3909a87d4bba06881",
+    "url": "/employee-app/static/css/main.9362c223.chunk.css"
+  },
+  {
+    "revision": "13f3909a87d4bba06881",
+    "url": "/employee-app/static/js/main.13f3909a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/employee-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "c75174c1fa328a7d8e5a",
+    "url": "/employee-app/static/js/2.c75174c1.chunk.js"
+  },
+  {
+    "revision": "67b34e92320b7a804bd1",
+    "url": "/employee-app/static/js/runtime~main.67b34e92.js"
+  },
+  {
+    "revision": "d7417041239dfea85cb0feaed1a261fc",
+    "url": "/employee-app/index.html"
+  }
+];
